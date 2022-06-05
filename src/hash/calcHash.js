aa8566613856c110c3ff73639756e743a01fbb6d
kayarez <tansteppp@gmail.com>
2022-06-05 12:42:14 +0200

    feat: implement hash subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,20 @@
+import * as crypto from 'crypto';
+import * as fs from 'node:fs/promises';
+
 export const calculateHash = async () => {
-    // Write your code here 
+
+    try {
+
+        const data = await fs.readFile('files/fileToCalculateHashFor.txt', { encoding: 'utf8' });
+        const hash = await crypto.createHash('sha256').update(data).digest('hex');
+        console.log(hash);
+        
+    } catch (error) {
+
+        console.error(error.message);
+        
+    }   
+
 };
+
+calculateHash();
